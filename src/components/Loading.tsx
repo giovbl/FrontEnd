2e287c6bbeb0d29ff08aaf84972c72377d19cbd8
Giovanni B. <giovanni.belluomo@outlook.it>
2026-03-21 12:40:49 +0100

    Better loading visualization

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,9 +1,10 @@
-import { Loader,Text } from "@mantine/core";
+import { Loader,Space,Text } from "@mantine/core";
 
 function Loading(){
     return (
         <>
             <Loader color="blue" type="dots"/>
+            <Space h='md'/>
             <Text>Caricamento in corso...</Text>
         </>
     )
