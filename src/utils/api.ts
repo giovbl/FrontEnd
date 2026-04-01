fe7b7839fd66d6d227435973a207a3b3223b1fbb
Giovanni B. <giovanni.belluomo@outlook.it>
2026-04-01 16:20:59 +0200

    Changed API url to ec2 instance one

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: 'ec2-16-171-205-83.eu-north-1.compute.amazonaws.com/api',
   headers: {
     'Content-Type': 'application/json'
   },
